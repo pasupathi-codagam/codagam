884b8584bd7800440f9a056accec845568f59991
psmpasu-19 <your-email@example.com>
2025-10-28 17:42:16 +0530

    ui fixed

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,8 +1,10 @@
 // Shared components exports
 export { default as NavigationButton } from "./NavigationButton";
 export { default as StatsCard } from "./StatsCard";
+export { default as SectionWrapper } from "./SectionWrapper";
 export { ClientLogoCarousel } from "./ClientLogoCarousel";
 export { ContactForm } from "./ContactForm";
+export { useScrollContainer } from "./ScrollContainer";
 export {
   useScrollAnimation,
   useStaggeredScrollAnimation,
